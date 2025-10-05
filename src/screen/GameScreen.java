2e57beefd4e1d08591200969a06aecbcad75c00c
minjun kim <minuun1024@naver.com>
2025-10-05 17:08:58 +0900

    feat: implement temporary item system
    # Conflicts:
    #	teams/KWAK.md

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -14,6 +14,7 @@ import entity.EnemyShip;
 import entity.EnemyShipFormation;
 import entity.Entity;
 import entity.Ship;
+import entity.Item;
 
 /**
  * Implements the game screen, where the action happens.
@@ -101,6 +102,10 @@ public class GameScreen extends Screen {
 		this.lives = gameState.getLivesRemaining();
 		if (this.bonusLife)
 			this.lives++;
+
+		// 아이템 추가 생명 적용
+		this.lives += Item.getExtraLives();
+
 		this.bulletsShot = gameState.getBulletsShot();
 		this.shipsDestroyed = gameState.getShipsDestroyed();
 	}
