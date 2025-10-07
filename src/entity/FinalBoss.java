6a5ad7518b4cfe2631c6e3144d22751db768c45f
rioluvy <s0ftrisus@gmail.com>
2025-10-08 00:18:17 +0900

    feat: implemented final boss object and movement pattern

---
+++ src/entity/FinalBoss.java
@@ -0,0 +1,117 @@
+package entity;
+
+import engine.DrawManager;
+import engine.Cooldown;
+import engine.Core;
+import java.awt.*;
+
+public class FinalBoss extends Entity{
+
+    public int healPoint;
+    private int Hp;
+    private final int pointValue;
+    private boolean isDestroy;
+    /** for move pattern **/
+    private int zigDirection = 1;
+    /** for move pattern **/
+    private boolean goingDown = true;
+
+    private Cooldown animationCooldown;
+    private Cooldown shootCooldown;
+
+    /** basic attribute of final boss **/
+    public FinalBoss(int positionX, int positionY){
+        super(positionX,positionY,100,80, Color.RED);
+        this.healPoint = 20;
+        this.Hp = healPoint;
+        this.pointValue = 1000;
+        this.spriteType = DrawManager.SpriteType.EnemyShipSpecial;
+        this.isDestroy = false;
+
+        this.animationCooldown = Core.getCooldown(500);
+        this.shootCooldown = Core.getCooldown(1000);
+    }
+
+    /** for vibrant moving with final boss **/
+    /** final boss spritetype is the same with special enemy and enemyshipA, because final boss spritetype have not yet implemented **/
+    /** becasue final boss is single object, moving and shooting pattern are included in update methods **/
+    public void update(){
+        if(this.animationCooldown.checkFinished()){
+            this.animationCooldown.reset();
+
+            if(this.spriteType == DrawManager.SpriteType.EnemyShipSpecial){
+                this.spriteType = DrawManager.SpriteType.EnemyShipA1;
+            } else{
+                this.spriteType = DrawManager.SpriteType.EnemyShipSpecial;
+            }
+        }
+        movePattern();
+        shoot();
+    }
+
+    /** decrease boss' healpoint **/
+    public void takeDamage(int damage){
+        this.healPoint -= damage;
+        if(this.healPoint <= 0){
+            this.destroy();
+        }
+    }
+
+    public int getPointValue(){
+        return this.pointValue;
+    }
+
+    /** movement pattern of final boss **/
+    public void movePattern(){
+        if(this.healPoint > this.Hp/2){
+            this.move(0,0);
+        }
+        else {
+            this.moveZigzag(3,2);
+        }
+    }
+
+    /** move zigzag **/
+    public void moveZigzag(int zigSpeed, int vertSpeed){
+        this.positionX += (this.zigDirection * zigSpeed);
+        if(this.positionX <= 0 || this.positionX >= 448-this.width){
+            this.zigDirection *= -1;
+        }
+
+        if(goingDown) {
+            this.positionY += vertSpeed;
+            if (this.positionY >= 280 - this.height) goingDown = false;
+        }
+        else {
+            this.positionY -= vertSpeed;
+            if(positionY <= 0) goingDown = true;
+        }
+    }
+
+    /** move simple **/
+    public void move(int distanceX, int distanceY){
+        this.positionX += distanceX;
+        this.positionY += distanceY;
+    }
+
+    /** not yet implemented **/
+    public void shoot(){
+        if(this.shootCooldown.checkFinished()){
+            this.shootCooldown.reset();
+            // 총알 객체 생성 로직
+        }
+    }
+
+    /** flag final boss' destroy **/
+    public void destroy(){
+        if(!this.isDestroy){
+            this.isDestroy = true;
+            this.spriteType = DrawManager.SpriteType.Explosion;
+        }
+    }
+
+    /** check final boss' destroy **/
+    public boolean isDestroyed(){
+        return this.isDestroy;
+    }
+}
