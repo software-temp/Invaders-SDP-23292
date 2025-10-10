d7163fd93e611a0da92712b4dcca1e42ed48a6d9
msssggg3-14 <ijaehu001@gmail.com>
2025-10-11 00:29:08 +0900

    <feat>/<moded : DrawManager.java, graphics>

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -111,7 +111,15 @@ public final class DrawManager {
 			spriteMap.put(SpriteType.EnemyShipC2, new boolean[12][8]);
 			spriteMap.put(SpriteType.EnemyShipSpecial, new boolean[16][7]);
 			spriteMap.put(SpriteType.Explosion, new boolean[13][7]);
-
+			spriteMap.put(SpriteType.Item_MultiShot, new boolean[5][5]);
+			spriteMap.put(SpriteType.Item_Atkspeed, new boolean[5][5]);
+			spriteMap.put(SpriteType.Item_Penetrate, new boolean[5][5]);
+			spriteMap.put(SpriteType.Item_Explode, new boolean[5][5]);
+			spriteMap.put(SpriteType.Item_Slow, new boolean[5][5]);
+			spriteMap.put(SpriteType.Item_Stop, new boolean[5][5]);
+			spriteMap.put(SpriteType.Item_Push, new boolean[5][5]);
+			spriteMap.put(SpriteType.Item_Shield, new boolean[5][5]);
+			spriteMap.put(SpriteType.Item_Heal, new boolean[5][5]);
 			fileManager.loadSprite(spriteMap);
 			logger.info("Finished loading the sprites.");
 
