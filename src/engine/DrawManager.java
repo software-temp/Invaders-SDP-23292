f625e7ee3859f881bf602477d945115f70a398b4
Antoine MAO <131959203+Anetlow@users.noreply.github.com>
2025-10-14 10:27:48 +0200

    Update DrawManager.java, retrieve deleted lines

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -115,6 +115,8 @@ public final class DrawManager {
 			spriteMap.put(SpriteType.EnemyShipC2, new boolean[12][8]);
 			spriteMap.put(SpriteType.EnemyShipSpecial, new boolean[16][7]);
 			spriteMap.put(SpriteType.Explosion, new boolean[13][7]);
+			spriteMap.put(SpriteType.SoundOn, new boolean[15][15]);
+			spriteMap.put(SpriteType.SoundOff, new boolean[15][15]);
 			spriteMap.put(SpriteType.Item_Explode, new boolean[5][5]);
 			spriteMap.put(SpriteType.Item_Slow, new boolean[5][5]);
 			spriteMap.put(SpriteType.Item_Stop, new boolean[5][5]);
